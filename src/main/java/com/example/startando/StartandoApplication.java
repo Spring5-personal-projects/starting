c67bd24d667ef58fdea5bc4f51edf21ce73a50c6
Jhonatan Mota <savaegt@gmail.com>
2020-11-24 07:20:36 -0300

    first commit

---
+++ src/main/java/com/example/startando/StartandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.startando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartandoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/startando/StartandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
